e773a6c977cebf0995b6652947bc739b845e0319
Paul Dudsdeemaytha <pdudsdee@sfu.ca>
2019-10-17 13:20:01 -0700

    added all notes and created a play sound function

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,72 @@
-import '2package:flutter/material.dart';
+import 'package:flutter/material.dart';
+import 'package:audioplayers/audio_cache.dart';
 
-void main() => runApp(XylophoneApp());
+void main() {
+  runApp(XylophoneApp());
+}
 
 class XylophoneApp extends StatelessWidget {
+  void playSound(int noteNumber) {
+    final player = AudioCache();
+    player.play('note$noteNumber.wav');
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
+          child: Column(children: <Widget>[
+            FlatButton(
+              color: Colors.red,
+              onPressed: () {
+                playSound(1);
+              },
+              child: Text('Press Me'),
+            ),
+            FlatButton(
+              color: Colors.blue,
+              onPressed: () {
+                playSound(2);
+              },
+              child: Text('Press Me'),
+            ),
+            FlatButton(
+              color: Colors.green,
+              onPressed: () {
+                playSound(3);
+              },
+              child: Text('Press Me'),
+            ),
+            FlatButton(
+              color: Colors.yellow,
+              onPressed: () {
+                playSound(4);
+              },
+              child: Text('Press Me'),
+            ),
+            FlatButton(
+              color: Colors.orange,
+              onPressed: () {
+                playSound(5);
+              },
+              child: Text('Press Me'),
+            ),
+            FlatButton(
+              color: Colors.teal,
+              onPressed: () {
+                playSound(6);
+              },
+              child: Text('Press Me'),
+            ),
+            FlatButton(
+              color: Colors.purple,
+              onPressed: () {
+                playSound(7);
+              },
+              child: Text('Press Me'),
+            ),
+          ]),
         ),
       ),
     );
